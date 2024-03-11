7350ac376cd326de44a8099660c782c9521d4a92
tpranjic-tgm <tpranjic@student.tgm.ac.at>
2024-03-11 22:32:05 +0100

    package.json cahnged

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 const API_URL = "https://socket.nidalee.party/";
-let dev = true;
+let dev = false;
 export const socket = io(dev ? "http://localhost:3001" : API_URL, {
   autoConnect: false,
   secure: true,
